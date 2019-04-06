18a7e8c411bc62262895fc7e232aca117a677462
LiXianyao <18976753003@163.com>
2019-04-06 10:49:18 +0800

    控制台部分上浮为公共；调整顺序，先处理\r\n（不然读取再转码后就没了）测试ok

--- brandInfo/csvReader.py
+++ brandInfo/csvReader.py
@@ -1,14 +1,11 @@
 #-*-coding:utf8-*-#
 import pandas as pd
 import codecs
+import sys
 import os
-import logging
-logger = logging.getLogger(__name__)
-logLevel = logging.DEBUG  ##正式运行的时候改成.INFO
-logger.setLevel(level=logLevel)
-console = logging.StreamHandler()
-console.setLevel(logLevel)
-logger.addHandler(console)
+sys.path.append('..')
+from consoleLogger import logger
+import traceback
 
 u"""
 分别读取两种csv文件，对调试时遇到的可能的数据错误进行处理
@@ -17,12 +14,23 @@ class CsvReader:
     def __init__(self):
         pass
 
-    def load_csv_to_pandas(self, csv_name, type):
-        new_csv_name = self.reStoreCsv(csv_name, type)
-        #self.process_csv_data(new_csv_name)
-        data = pd.read_csv(new_csv_name, encoding="utf-8", quotechar=None, quoting=3)
-        print data.head()
-        print data.shape
+    def load_csv_to_pandas(self, csv_name):
+        res_state = True
+        process_res = None
+        try:
+            clean_csv_name = self.process_csv_data(csv_name.encode("utf8"))
+            new_csv_name = self.reStoreCsv(clean_csv_name)
+            data = pd.read_csv(new_csv_name, encoding="utf-8", quotechar=None, quoting=3)
+            rows, columns = data.shape
+            logger.info(u"csv文件%s解析读取完成,共有%d行，%d列"%(csv_name, rows, columns))
+            process_res = data
+        except:
+            res_state = False
+            logger.error(u"csv解析时错误发生")
+            logger.error(traceback.format_exc())
+            process_res = u"文件解析错误，请检查csv文件的正确性、完整性（例如将其另存为utf8格式等）"
+        return res_state, process_res
+
 
     def process_csv_data(self, csv_name):
         u"""
@@ -33,11 +41,13 @@ class CsvReader:
         去掉\r：
          cat  注册商标基本信息.csv| sed ':a;N;$!ba;s/\r\n//g' | sed 's/\r//g'  > check.csv
         """
-        command = "cat " + csv_name + "| sed ':a;N;$!ba;s/\\r\\n//g' | sed 's/\\r//g'  >" + csv_name
-        #os.system(command)
-        logger.info(u"字符\\r与\\n消除完毕！")
+        clean_name = csv_name.replace(".csv", ".rm")
+        command = "cat " + csv_name + "| sed ':a;N;$!ba;s/\\r\\n//g' | sed 's/\\r//g'  >" + clean_name
+        os.system(command)
+        logger.info(u"字符\\r与\\r\\n消除完毕！")
+        return clean_name
 
-    def reStoreCsv(self, csv_name, type):
+    def reStoreCsv(self, csv_name):
         u"""
         对输入的csv文件进行处理，删除可能影响解析的因素，包括:
         编码异常
@@ -47,49 +57,51 @@ class CsvReader:
          cat  注册商标基本信息.csv| sed ':a;N;$!ba;s/\r\n//g' | sed 's/\r//g'  > check.csv
         """
         f_old = open(csv_name, "rU")      #读取原本的数据（逐行）
-        new_csv_name = type + "_reStore.csv"
+        #new_csv_name = type + "_reStore.csv"
+        new_csv_name = csv_name.replace(".rm", ".csv")
         f_new = codecs.open(new_csv_name, "w", "UTF-8")  #转存为UTF8格式
         cnt = 0
         cnt_error = 0
         cnt_blank = 0
-        last = ""
         save_lines = []
-        concate = False
+        concatenate = False
         while True:
             cnt += 1
             try:  ##尝试读取数据行
                 line = f_old.readline()
             except:
                 cnt_error += 1
-                logger.error(u"读取原始文件时转码错误，原因：存在不可解读为UTF-8的字符，已忽略此行数据,错误发生行号%d,最后一行有效数据为[%s]"%(cnt, last))
-                concate = False
+                logger.error(u"读取原始文件时转码错误，原因：存在不可解读为UTF-8的字符，已忽略此行数据,错误发生行号%d"%cnt)
+                concatenate = False
                 continue
 
             if not line:
                 break ##文件读完跳出
             if len(line) == 1:
-                concate = True
+                concatenate = True
                 cnt_blank += 1
                 continue    #跳过空行
 
             last = line
             try:  ##尝试转存数据行
-                if concate == True:
+                if concatenate == True:
                     save_lines[-1] += line.decode("utf8")
                 else:
                     save_lines.append(line.decode("utf8"))
             except:
-                logger.error(u"保存数据行时转码错误，原因：存在不可保存为UTF-8的字符,已忽略此行数据,错误发生行号%d,最后一行有效数据为" % cnt)
-                print last
-            concate = False
+                cnt_error += 1
+                logger.error(u"保存数据行时转码错误，原因：存在不可保存为UTF-8的字符,已忽略此行数据,错误发生行号%d" % cnt)
+            concatenate = False
 
         f_new.writelines(save_lines)
         logger.info(u"文件转码存储完成,原始文件共有%d行，其中转存出错%d行, 空白行%d行"%(cnt - 1, cnt_error, cnt_blank))
         f_old.close()
         f_new.close()
+        os.system("rm " + csv_name)
         return new_csv_name
 
 if __name__=="__main__":
     ##u"注册商标商品服务信息.csv"
     reader = CsvReader()
-    reader.load_csv_to_pandas(csv_name=u"注册商标基本信息.csv",type="info")
+    print reader.load_csv_to_pandas(csv_name=u"注册商标基本信息.csv")
+    print reader.load_csv_to_pandas(csv_name=u"注册商标商品服务信息.csv")
