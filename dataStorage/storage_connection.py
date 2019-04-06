b476bba89dff4d629833ff999966ce093098359c
LiXianyao <18976753003@163.com>
2019-04-06 11:11:10 +0800

    增加logger输出

--- dataStorage/storage_connection.py
+++ dataStorage/storage_connection.py
@@ -1,6 +1,7 @@
 #-*-coding:utf8-*-#
 import ConfigParser
 import redis
+from consoleLogger import logger
 
 class RedisConnection:
     def __init__(self, config_file="storage_redis.config"):
@@ -14,6 +15,7 @@ class RedisConnection:
         self.redis_pwd = self.cf.get("redis", "redis_pwd")
         ##创建redis连接及管道
         self.db = redis.StrictRedis(host=self.redis_ip, port=self.redis_port, db=self.redis_db, password=self.redis_pwd)
+        logger.info(u"已建立数据库连接，Host=%s:%s,db=%s" % (self.redis_ip, self.redis_port, self.redis_db))
         self.pipe = self.db.pipeline()
 
     def clear_redis_key(self, prefix):
@@ -21,6 +23,6 @@ class RedisConnection:
         for key in old_data:
             self.pipe.delete(key)
         self.pipe.execute()
-        print "delete keys of prefix '%s' num: %d" % (prefix, len(old_data))
+        logger.info("delete keys of prefix '%s' with num: %d" % (prefix, len(old_data)))
         del old_data[:]
 
