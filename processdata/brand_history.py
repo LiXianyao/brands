50ccea54e6d26b070b02e02312bb05a7f4e9483b
LiXianyao <18976753003@163.com>
2019-04-08 22:35:48 +0800

    复用数据库插入代码，创建历史数据记录表（为了统计）

--- processdata/brand_history.py
+++ processdata/brand_history.py
@@ -1,46 +1,58 @@
 # -*- coding: utf-8 -*-
 from sqlalchemy import Column,String, Integer, ForeignKey, Date, Text
-from database import Base,db_session
+from database import Base, db_session
 class BrandHistory(Base):
 
     #表名
     __tablename__ ='brand_history'
     #表结构
     id = Column(Integer, primary_key=True, autoincrement=True)
-    brand_name = Column(String(255))
-    product_list = Column(Text)
-    apply_date = Column(Date)
-    i18n_type = Column(Integer, nullable=False)
-    brand_status =  Column(Integer,nullable=True)
-    csv_name = Column(Integer, nullable=True)
+    brand_no = Column(String(10))
+    brand_name = Column(String(128))
+    apply_date = Column(String(8))
+    class_no = Column(Integer, nullable=False)
+    brand_status = Column(Integer, nullable=True)
+    insert_status = Column(Integer, nullable=True)#1是成功插入，2是图形或空跳过，3是名字异常跳过
     # 查询构造器、、、
     query = db_session.query_property()
 
-    def __init__(self, brand_name, product_list, apply_date, i18n_type, brand_status, csv_name):
+    def __init__(self, brand_no, brand_name, apply_date, class_no, brand_status, insert_status):
+        self.brand_no = brand_no
         self.apply_date = apply_date
-        self.product_list = product_list
+        self.class_no = class_no
         self.brand_name = brand_name
-        self.i18n_type = i18n_type
         self.brand_status = brand_status
-        self.csv_name = csv_name
+        self.insert_status = insert_status
 
     def __repr__(self):
         brand_dict = {
             u"brand_name": self.brand_name,
-            u"product_list": self.product_list,
-            u"apply_date":str(self.apply_date),
-            u"i18n_type":self.i18n_type,
-            u"brand_status":self.brand_status
+            u"brand_no": self.brand_no,
+            u"apply_date": str(self.apply_date),
+            u"class_no": self.class_no,
+            u"brand_status": self.brand_status,
+            u"insert_status": self.insert_status
         }
         return str(brand_dict)
 
     def __dir__(self):
         brand_dict = {
             u"brand_name": self.brand_name,
-            u"product_list": self.product_list,
+            u"brand_no": self.brand_no,
             u"apply_date": str(self.apply_date),
-            u"i18n_type": self.i18n_type,
-            u"brand_status": self.brand_status
+            u"class_no": self.class_no,
+            u"brand_status": self.brand_status,
+            u"insert_status": self.insert_status
         }
         return str(brand_dict)
 
+if __name__=="__main__":
+    new_record = BrandHistory('34738019', u"图形", "20181116", 13, 0, 1)
+    db_session.add(new_record)
+    new_record = BrandHistory('34738019', u"图形", "20181116", 13, 0, 1)
+    db_session.add(new_record)
+    new_record = BrandHistory('34738019', u"图形", "20181116", 13, 0, 1)
+    db_session.add(new_record)
+    new_record = BrandHistory('34738019', u"图形", "20181116", 13, 0, 1)
+    db_session.add(new_record)
+    db_session.commit()
--- processdata/database.py
+++ processdata/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine, MetaData, inspect
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-db_engine = create_engine('mysql://root:123456@10.109.246.96:3306/Brand?charset=utf8', convert_unicode=True)
+db_engine = create_engine('mysql://root:123456@10.109.246.96:3306/brand?charset=utf8', convert_unicode=True)
 ins = inspect(db_engine)
 db_metadata = MetaData(db_engine,reflect=True)
 db_inspector = inspect(db_engine)
