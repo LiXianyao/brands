2ab9359112b94c2648d682df297aea43b74ab3e7
buptlixianyao@163.com <18976753003@163.com>
2019-05-12 15:12:45 +0800

    增加可能在转换近似标签的时候修改近似概率

--- similarity/convert.py
+++ similarity/convert.py
@@ -38,7 +38,7 @@ def pinyin2(_str1,_str2):
         _str2 = ' '.join(lazy_pinyin(_str2))
 
     if _str1 == _str2:
-        return 9, u"声音相同字不同 8"     #声音相同字不同
+        return 9, u"声音相同字不同 8", None     #声音相同字不同
     _str1 = _str1.split(" ")
     _str2 = _str2.split(" ")
 
@@ -46,24 +46,24 @@ def pinyin2(_str1,_str2):
     ##输入名字被完全包含的情况：
     if lstr1 == maxNum:  #好中华 中华 名字前加字
         if s2_p == lstr2:
-            return 10, u"声音相同字不同前面加字 9"   #名字前加字
+            return 10, u"声音相同字不同前面加字 9", None   #名字前加字
 
         if s2_p == s1_p:   #中华好 中华 名字后加字
-            return 11, u"声音相同字不同后面加字 10"  # 名字后加字
+            return 11, u"声音相同字不同后面加字 10", None  # 名字后加字
         ##剩余情况为：输入名字是近似商标的一部分
-        return 12, u"名字的一部分声音相同字不同 12" #名字的一部分
+        return 12, u"名字的一部分声音相同字不同 12", None #名字的一部分
     else: ##输入商标只有部分适配
         if s2_p == lstr2 and lstr2 > maxNum:   #名字的一部分前面加文字：商标2的后缀是1的一部分
-            return 13, u"名字的一部分声音相同字不同，前面加文字 13" #名字的一部分前面加文字
+            return 13, u"名字的一部分声音相同字不同，前面加文字 13", None #名字的一部分前面加文字
         if s2_p == maxNum and lstr2 > maxNum:  # 商标2的前缀是1的一部分
-            return 14, u"名字的一部分声音相同字不同，后面加文字 14"#名字的一部分后面加文字
-        return 15, u"名字的一部分声音相同字不同，被包含 15"  # 名字中间加字
+            return 14, u"名字的一部分声音相同字不同，后面加文字 14", None #名字的一部分后面加文字
+        return 15, u"名字的一部分声音相同字不同，被包含 15", None  # 名字中间加字
 
-    """
+    u"""
     for index in range(len(_str1)):
         if index == 0 and _str1[index] == _str2:
-            return 10, u"声音相同字不同后面加字 10"   #声音相同字不同后面加字
-    return 9, u"声音相同字不同前面加字 9"    #声音相同字不同前面加字
+            return 10, u"声音相同字不同后面加字 10", None   #声音相同字不同后面加字
+    return 9, u"声音相同字不同前面加字 9", None    #声音相同字不同前面加字
     """
 
 def contain(_str1,_str2):
@@ -72,33 +72,33 @@ def contain(_str1,_str2):
     ##输入名字被完全包含的情况：
     if lstr1 == maxNum:  #好中华 中华 名字前加字
         if s2_p == lstr2:
-            return 2, u"名字前加字 2"   #名字前加字
+            return 2, u"名字前加字 2", None   #名字前加字
 
         if s2_p == s1_p:   #中华好 中华 名字后加字
-            return 3, u"名字后加字 3"  # 名字后加字
+            return 3, u"名字后加字 3", None  # 名字后加字
         ##剩余情况为：输入名字是近似商标的一部分
-        return 4, u"名字被包含 4" #名字的一部分
+        return 4, u"名字被包含 4", None #名字的一部分
     else: ##输入商标只有部分适配
         if s2_p == lstr2 and lstr2 > maxNum:   #名字的一部分前面加文字：商标2的后缀是1的一部分
-            return 5, u"名字的一部分前面加文字 5" #名字的一部分前面加文字
+            return 5, u"名字的一部分前面加文字 5", None #名字的一部分前面加文字
         if s2_p == maxNum and lstr2 > maxNum:  # 商标2的前缀是1的一部分
-            return 6, u"名字的一部分后面加文字 6"#名字的一部分后面加文字
-        return 7, u"名字的一部分被包含 7"  # 名字中间加字
+            return 6, u"名字的一部分后面加文字 6", None #名字的一部分后面加文字
+        return 7, u"名字的一部分被包含 7", None  # 名字中间加字
 
 
 def result(_str1,_str2,lable):
     _str1 = get_china_str(_str1)
     _str2 = get_china_str(_str2)
     if _str1 == _str2:
-        return 1, u"名字完全相同 1"
+        return 1, u"名字完全相同 1", 0.
     elif lable == "汉字字形相似度":
-        return 8, u"字形相近 8"
+        return 8, u"字形相近 8", None
     elif lable == "拼音相似度":
         return pinyin2(_str1,_str2)
     elif lable == "汉字包含被包含":
         return contain(_str1,_str2)
     else:
-        return 16, lable + u" 16"
+        return 16, lable + u" 16", None
 
 if __name__ == "__main__":
     print "%s"% result(u"中华", u"中华好", "拼音相似度")[1]
--- train/similarName.py
+++ train/similarName.py
@@ -1,5 +1,7 @@
 #-*-coding:utf8-*-#
 from similarity import convert
+from similarity.compute import similar_gate_low
+
 ###各个特征属性的名字（按构造得到的特征数据的顺序）
 attribute_title = [u"汉字编辑距离相似度", u"拼音相似度", u"汉字包含被包含",
                    u"汉字排列组合", u"汉字含义相近", u"汉字字形相似度",
@@ -32,7 +34,7 @@ class similarName:
         self.register_no = register_no
         self.calMaxAttri(attriList)
         self.convertTag(compareName)
-        #self.rate = predict_rate
+        self.predict_rate = predict_rate
 
     ###确定优先级最高的特征类型
     def calMaxAttri(self, attriList):
@@ -43,7 +45,7 @@ class similarName:
         #    print title , rate
         (self.rate, self.rateTitle) = (None, None)
         for (rate, title) in attriList:
-            if rate >= 0.67:  ###先按照优先级顺序找第一个满足阈值的类别
+            if rate >= similar_gate_low:  ###先按照优先级顺序找第一个满足阈值的类别
                 (self.rate, self.rateTitle) = (rate, title)
                 break
 
@@ -53,10 +55,13 @@ class similarName:
 
     ##转化特征类型为对应的标记
     def convertTag(self, compareName):
-        tag, tagName = convert.result(compareName, self.name, self.rateTitle)
+        tag, tagName, predictChange = convert.result(compareName, self.name, self.rateTitle)
         del self.rateTitle
         self.tag = [tag]
         #self.tagName = tagName ###之后可以删
+        u""" 一些情况下可能需要更新概率 """
+        if predictChange != None:
+            self.predict_rate = predictChange
 
     def __repr__(self):
         return repr((self.__dict__))
