3b5bc0f27e32e2b9c00b55daa5be4ab60ca20812
buptlixianyao@163.com <18976753003@163.com>
2019-05-12 13:20:10 +0800

    有点不妥，以防万一对训练数据做一定的洗牌

--- train/trans_train_data_mysql.py
+++ train/trans_train_data_mysql.py
@@ -49,16 +49,18 @@ def train_Data(train_data_list, taskId=taskId):
     print " 0 has %d, 1 has %d "%(len(input_lines[0]),len(input_lines[1]))
     u""" 取两者之小作为 """
     limit = int(min(len(input_lines[0]),len(input_lines[1])) * 1.1)
+
+    seed = 1
+    np.random.seed(seed)
+    np.random.shuffle(input_lines[0])
+    np.random.shuffle(input_lines[1])
+
     train_lines = input_lines[0][:limit]
     len_false = len(train_lines)
     train_lines.extend(input_lines[1][:limit])
     len_true = len(train_lines) - len_false
-    print "after cut, 0 has %d, 1 has %d " % (len_false, len_true)
-
-    seed = 1
-    np.random.seed(seed)
     np.random.shuffle(train_lines)
-    print "train size %d"%(len(train_lines))
+    print "after cut, 0 has %d, 1 has %d,train size %d " % (len_false, len_true, len(train_lines))
 
     save_input_file(train_lines,"../train/data/input/" + input_train_file_name)
 
